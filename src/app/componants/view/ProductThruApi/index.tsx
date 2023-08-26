462caecb28582ef012fa8f79181b5ce6606ab855
Khuram Hanif <Khuramhanif42@gmail.com>
2023-08-26 11:56:41 +0500

    resolving some errors

--- src/app/componants/view/ProductThruApi/index.tsx
+++ src/app/componants/view/ProductThruApi/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import React from "react";
 
-const ProductthruApi = ({ProductData}:any) => {
-  return (
-    <div>{ProductData[0].title}</div>
-  )
-}
-
-export default ProductthruApi
+const ProductthruApi = ({ ProductData }: any) => {
+  return <div>{ProductData[0].title}</div>;
+};
+export default ProductthruApi;
